d3a4f56779c67febc32cea15857e0b3258bb6c44
MscMin <selleeeggl@gmail.com>
2022-11-02 04:29:57 +0900

    typo correction

--- Calculator.py
+++ Calculator.py
@@ -18,7 +18,7 @@ while True:
     choice = input("Enter choice(1/2/3/4): ")
 
     if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first numbeyesr: "))
+        num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
